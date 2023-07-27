4fb4aae1012f0f23ce862245369599976ac42b91
clee01 <1726241244@qq.com>
2023-07-27 20:56:19 +0800

    dev: 递归算法解题

---
+++ recursion/maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * 题目来源：https://leetcode.com/explore/learn/card/recursion-i/256/complexity-analysis/2375/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
