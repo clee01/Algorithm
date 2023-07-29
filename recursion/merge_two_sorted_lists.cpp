6099a09fce1dc44e8c306d7d08f9d60a6e526b58
clee01 <1726241244@qq.com>
2023-07-29 22:49:56 +0800

    dev: 递归算法解题

---
+++ recursion/merge_two_sorted_lists.cpp
@@ -0,0 +1,34 @@
+/**
+ * 题目来源：https://leetcode.com/explore/learn/card/recursion-i/253/conclusion/2382/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) {
+            return list2;
+        }
+
+        if (!list2) {
+            return list1;
+        }
+
+        if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+    }
+};
