c47918554f063d4b9eb061b236ffc8b037bc5c6e
clee01 <1726241244@qq.com>
2023-08-04 23:58:20 +0800

    dev: 分治算法解题

--- divide_and_conquer/maximum_subarray.cpp
+++ divide_and_conquer/maximum_subarray.cpp
@@ -5,6 +5,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+        return doMax(nums, 0, nums.size() - 1);
+    }
+
+    int doMax(vector<int>& nums, int start, int end) {
+        if (start == end) {
+            return nums[start];
+        }
+        if (nums[start] > 0) {
+            nums[start + 1] += nums[start];
+        }
+        return std::max(nums[start], doMax(nums, start + 1, end));
     }
 };
